2ec232d2d98cbf283fbe7d6ecdbb36db2429ba15
joeoakes <joe.oakes@gmail.com>
2019-03-08 16:56:49 -0500

    update to main

--- src/edu/psu/ist/Main.java
+++ src/edu/psu/ist/Main.java
@@ -1,8 +1,25 @@
+
 package edu.psu.ist;
+
+import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        ArrayList<Customer> cList = new ArrayList<>();
+        ArrayList<Menu> mList = new ArrayList<>();
+        ArrayList<Order> oList = new ArrayList<>();
+        ArrayList<Transaction> tList = new ArrayList<>();
+
+        Customer cust1 = new Customer(1);
+        Menu menu1 = new Menu(1);
+        Order order1 = new Order(1);
+        Transaction trans1 = new Transaction(1);
+
+        cList.add(cust1);
+        mList.add(menu1);
+        oList.add(order1);
+        tList.add(trans1);
     }
 }
