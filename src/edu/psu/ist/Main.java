4b4627d324f19b9b71fbdeb3125d136a4be2f750
joeoakes <joe.oakes@gmail.com>
2019-03-13 21:10:08 -0400

    update to main

--- src/edu/psu/ist/Main.java
+++ src/edu/psu/ist/Main.java
@@ -10,27 +10,66 @@ Rev: 2
  */
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
+        final char EXIT_CODE = 'E';
+        final char CUST_CODE = 'C';
+        final char MENU_CODE = 'M';
+        final char ORDE_CODE = 'O';
+        final char TRAN_CODE = 'T';
+        final char HELP_CODE = '?';
+        char userAction;
+        final String PROMPT_ACTION = "Add 'C'ustomer, List 'M'enu, Add 'O'rder, List 'T'ransaction or 'E'xit: ";
         ArrayList<Customer> cList = new ArrayList<>();
         ArrayList<Menu> mList = new ArrayList<>();
         ArrayList<Order> oList = new ArrayList<>();
         ArrayList<Transaction> tList = new ArrayList<>();
 
         Customer cust1 = new Customer(1);
-        Menu menu1 = new Menu(1);
         Order order1 = new Order(1);
         Transaction trans1 = new Transaction(1);
 
-        cList.add(cust1);
+        Menu menu1 = new Menu(1, "Plain");
+        Menu menu2 = new Menu(2, "Meat");
+        Menu menu3 = new Menu(3, "Extra");
+        Menu menu4 = new Menu(4, "Veg");
+
         mList.add(menu1);
+        mList.add(menu2);
+        mList.add(menu3);
+        mList.add(menu4);
+
+        cList.add(cust1);
         oList.add(order1);
         tList.add(trans1);
-        Customer custReturn = cList.get(0);
-        System.out.println(custReturn.getcustomerId());
 
+        userAction = getAction(PROMPT_ACTION);
+
+        while (userAction != EXIT_CODE) {
+            switch(userAction) {
+                case CUST_CODE : //addCustomer();
+                    break;
+                case MENU_CODE : Menu.listMenu(mList);
+                    break;
+                case ORDE_CODE : //addOrders();
+                    break;
+                case TRAN_CODE : //listTransactions();
+                    break;
+            }
+
+            userAction = getAction(PROMPT_ACTION);
+        }
+    }
+    public static char getAction(String prompt) {
+        Scanner scnr = new Scanner(System.in);
+        String answer = "";
+        System.out.println(prompt);
+        answer = scnr.nextLine().toUpperCase() + " ";
+        char firstChar = answer.charAt(0);
+        return firstChar;
     }
 }
--- src/edu/psu/ist/Menu.java
+++ src/edu/psu/ist/Menu.java
@@ -1,16 +1,29 @@
 package edu.psu.ist;
 
+import java.util.ArrayList;
+
 public class Menu {
 
     //Class Level Variables - Protect the data
     private int menuId;
+    private String menuItem;
 
     //Constructor Method
-    public Menu(int _menuId){
+    public Menu(int _menuId, String _menuItem){
         this.menuId = _menuId;
+        this.menuItem = _menuItem;
     }
 
     //Setters and Getters
     public int getmenuId() { return menuId; }
     public void setmenuId(int _menuId) {this.menuId = _menuId;}
+
+    public String getmenuItem() { return menuItem; }
+    public void setmenuItem(String _menuItem) {this.menuItem = _menuItem;}
+
+    public static void listMenu(ArrayList<Menu> mList){
+        for (Menu menu: mList){
+            System.out.println(menu.getmenuItem());
+        }
+    }
 }
